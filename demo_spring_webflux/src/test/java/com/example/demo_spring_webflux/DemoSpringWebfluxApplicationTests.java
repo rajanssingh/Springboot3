4c3c6f6931ea14b35052556b1af60d4ba0622727
Rajan Singh <rjnkumar628@gmail.com>
2023-05-21 22:46:37 +0530

    Initial Commit

---
+++ demo_spring_webflux/src/test/java/com/example/demo_spring_webflux/DemoSpringWebfluxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo_spring_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //
+    }
+
+}
